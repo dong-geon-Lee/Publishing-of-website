551114a234535119f8583ea101655f2a8fcab941
developer <ch9716@naver.com>
2024-09-08 18:56:24 +0900

    Destination 섹션 목업 구현 완료

--- 2-Travosy/script.js
+++ 2-Travosy/script.js
@@ -1,1 +1,6 @@
+const header = document.querySelector("header");
 
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY, "상태");
+  header.classList.toggle("sticky", window.scrollY > 180);
+});
